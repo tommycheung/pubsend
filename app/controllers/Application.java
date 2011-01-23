45b988ffd1ce43b4fea72d7e3591b731e8c8700e
tommycheung <457167110@qq.com>
2011-01-23 14:32:58 +0800

    tommy first commit

---
+++ app/controllers/Application.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import play.mvc.Controller;
+
+public class Application extends Controller {
+
+    public static void index() {
+        //需要
+        render();
+    }
+
+}
---
+++ app/controllers/Home.java
@@ -0,0 +1,10 @@
+package controllers;
+
+public class Home extends Application {
+
+    //主页
+    public static void index() {
+        //新闻
+        render();
+    }
+}
